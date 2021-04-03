c6b24838b41cd1cda0a6888ba04b85c5991c81d7
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-03 16:28:48 +0200

    feat: Struct for the snake and the field

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::time::Duration;
+use std::thread::sleep;
+
+pub struct Snake{
+    head_position: [i32; i32],
+    body: [[i32; i32]],
+}
+
+pub struct Field{
+    size: [i32; i32]
+}
+
+
+fn main() {
+}
